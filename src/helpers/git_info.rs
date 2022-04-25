733218a347d8fd380642cedbb2aa85ca5c4ec189
kuqmua <serezhashmakov@outlook.com>
2022-04-25 20:04:50 +0300

    git_info

---
+++ src/helpers/git_info.rs
@@ -0,0 +1,7 @@
+use tufa_common::helpers::git::git_info::GitInformation;
+use lazy_static::lazy_static;
+use crate::constants::PROJECT_NAME;
+
+lazy_static! {
+    pub static ref GIT_INFO: GitInformation = GitInformation::get_git_commit_info("./", PROJECT_NAME);
+}
